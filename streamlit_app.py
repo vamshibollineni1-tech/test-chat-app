f4a1be02bf3ff626e9ea68e0f56eded4585b64dd
vamshibollineni1-tech <vamshibollineni1@gmail.com>
2025-09-06 14:28:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,21 @@
+#pip install streamlit
+
+# .streamlit/secrets.toml
+OPENAI_API_KEY = "YOUR_API_KEY"
+
 import streamlit as st
+from openai import OpenAI
 
-st.title("Echo Bot")
+st.title("ChatGPT-like clone")
 
-# with st.chat_message("user"):
-#     st.markdown("**User 👤:** Hello!")
+# Set OpenAI API key from Streamlit secrets
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-# with st.chat_message("assistant"):
-#     st.markdown("**Assistant 🤖:** Hi there!")
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-# prompt = st.chat_input("Say something")
-# if prompt:
-#     st.write(f"User has sent the following prompt: {prompt}")
-
+# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -19,10 +23,24 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-# React to user input
+
+# Accept user input
 if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown("**User 👤:** prompt")
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
