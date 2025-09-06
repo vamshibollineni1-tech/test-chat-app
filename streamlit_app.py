f7a1ac4a952198f4f734974b7094fccb4c35a322
vamshibollineni1-tech <vamshibollineni1@gmail.com>
2025-09-06 13:56:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ with st.chat_message("user"):
 
 with st.chat_message("assistant"):
     st.markdown("**Assistant 🤖:** Hi there!")
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
